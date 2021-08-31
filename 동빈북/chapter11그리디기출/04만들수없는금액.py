79292eefc118037e37d0bdadfbfd7a692d7f952f
hojin9car <hojin9car@gmail.com>
2021-08-31 13:22:52 +0900

    주석달고

--- 동빈북/chapter11그리디기출/04만들수없는금액.py
+++ 동빈북/chapter11그리디기출/04만들수없는금액.py
@@ -1,27 +1,27 @@
-n = int(input())
+n = int(input())  # n개의 동전. 
 
 coin = list(map(int, input().split()))
 
-money = coin  # 만들 수 있는 금액 리스트
+money = coin  # 만들 수 있는 금액 리스트, 동전 하나로 만들 수 있는 금액을 먼저 복사해놓고.
 
-for i in range(n):
-    sum = coin[i]
+for i in range(n):  
+    sum = coin[i]  # 
     for j in range(n):
-        if i == j:
+        if i == j:  # 가지고있는 동전으로 만들 수 있는 금액을 찾고있으므로 자기자신과 더할 수 없다.
             pass
         else:
             sum += coin[j]
             money.append(sum)
 
-money = set(money)
+money = set(money)  # set으로 중복값 제거.
 
 print("만들 수 있는 금액: ",money)
 
-min_money = 1
+min_money = 1  # 0원은 없으니까. 1원부터시작.
 
-for i in range(1, max(money)):
-    if i not in money:
-        min_money = i
+for i in range(1, max(money)):  # 1부터 시작. 만들수있는금액 중 최대 금액 까지.
+    if i not in money:  # money 안에 없는 i 값이 걸리면 그것이 만들수없는 금액.
+        min_money = i  # 1부터 시작했으니 걸리면 그것이 최소값.
         break
     
 print(min_money)
--- 동빈북/chapter11그리디기출/05볼링공고르기.py
+++ 동빈북/chapter11그리디기출/05볼링공고르기.py
@@ -1,4 +1,4 @@
-n, m = map(int, input().split())
+n, m = map(int, input().split()) 
 
 list_ball = list(map(int, input().split()))
 
