896ff77f91a201215fa1a9439e803ea9851bbdb8
hojin9car <hojin9car@gmail.com>
2021-08-31 13:30:33 +0900

    05.py 주석

--- 동빈북/chapter11그리디기출/05볼링공고르기.py
+++ 동빈북/chapter11그리디기출/05볼링공고르기.py
@@ -1,4 +1,4 @@
-n, m = map(int, input().split()) 
+n, m = map(int, input().split())  # n 공의 갯수, m 공의 최대무게.
 
 list_ball = list(map(int, input().split()))
 
@@ -6,10 +6,10 @@ cases = []
 
 for i in range(n):
     for j in range(i, n):
-        if i == j:
+        if i == j:  # 서로 같은 번호의 공 고르면 패스.
             pass
         else:
-            if list_ball[i] == list_ball[j]:
+            if list_ball[i] == list_ball[j]:  # 서로 같은 무게의 공 고르면 패스.  
                 pass
             else:
                 cases.append([list_ball[i],list_ball[j]])
