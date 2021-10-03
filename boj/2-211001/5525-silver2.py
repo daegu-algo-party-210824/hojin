47add3e5cb319e1f39504ca901baa9e04c344631
hojin9car <hojin9car@gmail.com>
2021-10-03 16:32:22 +0900

    5525 answer

--- boj/2-211001/5525-silver2.py
+++ boj/2-211001/5525-silver2.py
@@ -1,6 +1,47 @@
 # https://www.acmicpc.net/problem/5525
 
 import sys
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+M = int(input().rstrip())
+S = input().rstrip()
+
+answer = 0
+count = 0
+i = 1
+
+while i < M - 1:
+    if S[i-1] == "I" and S[i] == "O" and S[i+1] == "I":
+        count += 1
+        if count == N:
+            count -= 1
+            answer += 1
+        i += 1
+    else:
+        count = 0
+    i += 1
+
+print(answer)
+
+
+"""
+1
+13
+OOIOIOIOIIOII
+
+출력 4
+
+2
+13
+OOIOIOIOIIOII
+
+출력 2
+"""
+
+"""
+
+import sys
 
 n = int(sys.stdin.readline())  # O의 갯수 Pn
 
@@ -32,22 +73,8 @@ for i in range(start_index, m - (n * 2)):
     else:
         if validation(i) is True:
             count += 1
-            i += 2
         else:
             pass
 
 print(count)
-
-"""
-1
-13
-OOIOIOIOIIOII
-
-출력 4
-
-2
-13
-OOIOIOIOIIOII
-
-출력 2
 """
